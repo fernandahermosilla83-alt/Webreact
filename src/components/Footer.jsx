d75ffddeae0742dda744cab7766d229f1474c12b
fernandahermosilla83-alt <fernandahermosilla83@gmail.com>
2026-02-16 18:15:44 -0300

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer style={{padding:20,textAlign:"center",opacity:0.7}}>
+      © {new Date().getFullYear()} Guías Fitness
+    </footer>
+  );
+};
+
+export default Footer;
