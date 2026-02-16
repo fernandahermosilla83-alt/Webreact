f31809e19ec33b0d79ae67ec07cb023fdd519379
fernandahermosilla83-alt <fernandahermosilla83@gmail.com>
2026-02-16 18:52:22 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import GuideDetail from "./pages/GuideDetail";
 import NotFound from "./pages/NotFound";
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/guia/:id" element={<GuideDetail />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-);
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Esta es la página principal con Hero, Catálogo y FAQ */}
+        <Route path="/" element={<Index />} />
+        
+        {/* Esta es la página de detalle para cada guía */}
+        <Route path="/guia/:id" element={<GuideDetail />} />
+        
+        {/* Página de error 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
+
