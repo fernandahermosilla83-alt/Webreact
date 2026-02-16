3cd5a8e5326d7a45e4356e0ae13d0741a433fc62
fernandahermosilla83-alt <fernandahermosilla83@gmail.com>
2026-02-16 17:51:06 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import GuideDetail from "./pages/GuideDetail";
+import NotFound from "./pages/NotFound";
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/guia/:id" element={<GuideDetail />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
